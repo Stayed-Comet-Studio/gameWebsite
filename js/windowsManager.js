0f5c2b9eb170ea087d9153de785a815f21b47bfa
ZhangXiurui <cardidi@live.cn>
2018-09-08 18:48:35 +0800

    Rebuild browser level check.

--- js/windowsManager.js
+++ js/windowsManager.js
@@ -3,15 +3,13 @@ window.onresize = pageReSize;
 let html = document.documentElement;
 let body = document.getElementById("context");
 let sizeAlert = document.getElementById("sizeAlerts");
-let browserAlerts = document.getElementById("browserAlerts");
-let browserStatus = getExplore();
 
 function pageOnLoad() {
     pageContainCheck();
     coverReSize();
     document.getElementById("Loading").style.display = "none";
     body.style.display = "block";
-    //(!browserStatus) && blockBrowser();
+    checkBrowserLevel() && blockBrowser();
 }
 
 function pageReSize() {
@@ -21,7 +19,7 @@ function pageReSize() {
 
 function blockBrowser() {
     body.style.display = "none";
-    browserAlerts.style.display = "block";
+    document.getElementById("browserAlerts").style.display = "block";
 }
 
 function pageContainCheck() {
@@ -32,42 +30,13 @@ function pageContainCheck() {
         sizeAlert.style.display = "none";
         body.style.display = "block";
     }
-    // (!browserStatus) && blockBrowser(); //Should be rebuild.
 }
 
-function getExplore() {
-    let Sys = {};
-    let ua = navigator.userAgent.toLowerCase();
-    let s;
-    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1] :
-        (s = ua.match(/msie ([\d\.]+)/)) ? Sys.ie = s[1] :
-            (s = ua.match(/edge\/([\d\.]+)/)) ? Sys.edge = s[1] :
-                (s = ua.match(/firefox\/([\d\.]+)/)) ? Sys.firefox = s[1] :
-                    (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? Sys.opera = s[1] :
-                        (s = ua.match(/chrome\/([\d\.]+)/)) ? Sys.chrome = s[1] :
-                            (s = ua.match(/version\/([\d\.]+).*safari/)) ? Sys.safari = s[1] : 0;
-    //If Browser's version is right,return true.
-    if (Sys.ie < 9) {
-        alert(Sys.ie);
-        return (false);
-    }
-    if (Sys.edge) return (true);
-    if (Sys.firefox < 53) {
-        alert(Sys.firefox);
-        return (false);
-    }
-    if (Sys.chrome < 54) {
-        alert(Sys.chrome);
-        return (false);
-    }
-    if (Sys.opera < 37) {
-        alert(Sys.opera);
-        return (false);
-    }
-    if (Sys.safari < 4) {
-        alert(Sys.safari);
-        return (false);
-    }
+function checkBrowserLevel() {
+    let browser = navigator.appName;
+    let b_version = navigator.appVersion;
+    let version = parseFloat(b_version);
+    if ((browser == "Netscape" || browser == "Microsoft Internet Explorer") && (version > 4)) return false;
     return true;
 }
 
